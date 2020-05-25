baa74db04a7c354cf68c423972c27c2f171f3dca
William Kwon <williamkwon97@gmail.com>
2020-05-25 17:35:11 -0500

    commit

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -5,7 +5,6 @@ exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-about-js": () => import("./../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-contact-js": () => import("./../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-header-js": () => import("./../src/pages/header.js" /* webpackChunkName: "component---src-pages-header-js" */),
   "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-my-files-js": () => import("./../src/pages/my-files.js" /* webpackChunkName: "component---src-pages-my-files-js" */)
 }
